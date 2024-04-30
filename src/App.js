d2d7adb1e5714a38784653d42b7883023fe34a04
varsha <varshaanand103@gmail.com>
2024-04-30 12:36:02 +0530

    mmm

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import react from "react";
+import "./App.css";
+
+function App()
+{
+return <div><center><div className="heading"><h1>ABOUT ME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;MY WORK&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;CONTACT&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h1></div>
+<div className="h2"><h2>I&nbsp;&nbsp;&nbsp;SAW&nbsp;&nbsp;&nbsp;THE&nbsp;&nbsp;&nbsp;SIGN</h2></div>
+<p><div className="para">and it opened up my eyes.One of my favourite things to do is to capture the american nostalgia of signage.They tellus where we're going or where we've been.They express a mood or saying can even just be a shape.But it's the vintage,
+neon,and big light bulbed signs that say so much more.They aren't just your average square shapes with letters on it.
+some actual thought and planning went into the making of the sign.Oh and the lost are of advertising with neon signs
+that display motion is always a fun find! Such a lost art in my opinion.LED just hurts my eyes....doesn't it?</div>
+</p>
+<div>
+  <img src="https://media.licdn.com/dms/image/D5622AQF9ZitxIVyWvQ/feedshare-shrink_800/0/1701479686868?e=2147483647&v=beta&t=3YiiYpVgOU2yybzlb56iKZoR9_Cx3jYJGy7m6qSksMA"></img>
+  </div>
+  <div className="row ">
+  <img className="ctrl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTLl0c9hw0gJxj_dwOL7-Rcwnz4SyQhImjIQYU4vhH6sm9KXDFKIQZFqNxesdKgU0VaQw&usqp=CAU"></img>
+  <img className="ctrl" src="https://c4.wallpaperflare.com/wallpaper/819/954/385/enter-neon-photography-signs-wallpaper-preview.jpg"></img>
+  <img className="ctrl" src="https://mobstatus.com/wp-content/uploads/2021/08/Loving-You-Is-A-Losing-game-status.jpg"></img>
+  </div>
+  <div className="redu">
+  <img className="caps" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTLl0c9hw0gJxj_dwOL7-Rcwnz4SyQhImjIQYU4vhH6sm9KXDFKIQZFqNxesdKgU0VaQw&usqp=CAU"></img>
+  <img className="caps" src="https://c4.wallpaperflare.com/wallpaper/819/954/385/enter-neon-photography-signs-wallpaper-preview.jpg"></img>
+  <img className="caps"  src="https://mobstatus.com/wp-content/uploads/2021/08/Loving-You-Is-A-Losing-game-status.jpg"></img> 
+  </div>
+</center>
+</div>
+
+
+
+
+
+
+}
+export default App
